9291d617c7a4fbe19e6a3b8dd78c3f29d2d0ed45
sameer90 <sameersharma>
2015-06-22 22:07:32 -0400

    Final Version and fully Tested.

--- SS-SlotMachine/Scripts/game.ts
+++ SS-SlotMachine/Scripts/game.ts
@@ -39,6 +39,7 @@ var jackpot = 10000;
 var minBet = 0;
 var maxBet = 0;
 var defaultamount = 1000;
+var remainingvalue = 1000;
 var winvaluetxt = 0;
 var totalBetValue = 0;
 var curBetValue = 0;
@@ -56,6 +57,10 @@ var barScore = 30;
 var winScore = 0;
 var winArray = new Array();
 
+var timer1, timer2;
+var timerstatus = false;
+var objj, obj1, obj2;
+var totalBetBeforeReset = 0;
 
 // create a reference
 var spinButton: objects.Button;
@@ -117,45 +122,53 @@ function checkRange(value, lowerBounds, upperBounds) {
 function spinButtonClicked() {
     timerstatus = false;
     for (var spin = 0; spin < 3; spin++) {   
-        var rand = Math.floor((Math.random() * 75) + 1);
+        var rand = Math.floor((Math.random() * 90) + 1);
         switch (rand) {
             case checkRange(rand, 1, 20):
-                createImage("blank.png", spin);
-                break;
-            case checkRange(rand, 21, 30):
                 createImage("banana.png", spin);
-                winArray[spin] = "banana";   
+                winArray[spin] = "banana"; 
                 break;
-            case checkRange(rand, 31, 38):
-                createImage("bar.png",spin);
-                winArray[spin] = "bar";
-                break;
-            case checkRange(rand, 39, 45):
+            case checkRange(rand, 20, 30):
                 createImage("bigwin.png", spin);
                 winArray[spin] = "bigwin";
                 break;
-            case checkRange(rand, 46, 50):
+            case checkRange(rand, 31, 33):
+                createImage("blank.png", spin);
+                  
+                break;
+            case checkRange(rand, 34, 42):
+                createImage("bar.png",spin);
+                winArray[spin] = "bar";
+                break;
+            
+            case checkRange(rand, 43, 50):
                 createImage("eggplant.png", spin);
                 winArray[spin] = "eggplant";
                 break;
-            case checkRange(rand, 51, 53):
-                createImage("lemon.png",spin);
+            case checkRange(rand, 51, 64):
+                createImage("lemon.png", spin);
+                winArray[spin] = "lemon"; 
                 break;
-            case checkRange(rand, 54, 58):
-                createImage("melon.png",spin);
+            case checkRange(rand, 65, 72):
+                createImage("melon.png", spin);
+                winArray[spin] = "melon"; 
                 break;             
-            case checkRange(rand, 59, 62):
+            case checkRange(rand, 73, 79):
                 createImage("orange.png",spin);
+                winArray[spin] = "orange"; 
                 break;
-            case checkRange(rand, 63, 66):
-                createImage("pee.png",spin);
+            case checkRange(rand, 80, 85):
+                createImage("pee.png", spin);
+                winArray[spin] = "pee"; 
                 break;
-            case checkRange(rand, 67, 75):
-                createImage("seven.png",spin);
+            case checkRange(rand, 86, 90):
+                createImage("seven.png", spin);
+                winArray[spin] = "seven"; 
                 break;
             
         }
     }
+    calulateWIN();
 }
 
 // Our Main Game Function
@@ -208,8 +221,10 @@ function resetValue() {
     minBet = 0;
     maxBet = 0;
     defaultamount = 1000;
+    remainingvalue = 1000;
     winvaluetxt = 0;
     totalBetValue = 0;
+    totalBetBeforeReset = 0;
 
      stage.removeChild(jackpotText);
     stage.removeChild(accountText);
@@ -261,81 +276,91 @@ function resetValue() {
 
 }
 function doBet30() {
-    if (totalBetValue < defaultamount) {
-
-        totalBetValue += 30;
-
-        stage.removeChild(accountText);
-        stage.removeChild(totalBetText);
-        stage.removeChild(bit30Text);
-
-        maxBet += 30;
-        bit30Text = new createjs.Text(maxBet.toString(), "20px Consolas", "#FFFFFF");
-        bit30Text.x = 300;
-        bit30Text.y = 238;
-
-        totalBetText = new createjs.Text(totalBetValue.toString(), "20px Consolas", "#FFFFFF");
-        totalBetText.x = 243;
-        totalBetText.y = 300;
-
-        totalText = new createjs.Text("Total", "20px Consolas", "#FFFFFF");
-        totalText.x = 228;
-        totalText.y = 270;
-
-        var remainingamount = defaultamount - totalBetValue;
-
-        accountText = new createjs.Text(remainingamount.toString(), "20px Consolas", "#FFFFFF");
-        accountText.x = 65;
-        accountText.y = 238;
-
-        stage.addChild(accountText);
-        stage.addChild(totalText);
-
-        stage.addChild(totalBetText);
-        stage.addChild(bit30Text);
+    if (timerstatus == true) {
+        alert("Wait until Spinning Stops");
     }
     else {
-        alert("You have no money");
+
+        if (totalBetBeforeReset+30 < defaultamount) {
+
+            totalBetValue += 30;
+            totalBetBeforeReset += 30;
+            stage.removeChild(accountText);
+            stage.removeChild(totalBetText);
+            stage.removeChild(bit30Text);
+
+            maxBet += 30;
+            bit30Text = new createjs.Text(maxBet.toString(), "20px Consolas", "#FFFFFF");
+            bit30Text.x = 300;
+            bit30Text.y = 238;
+
+            totalBetText = new createjs.Text(totalBetValue.toString(), "20px Consolas", "#FFFFFF");
+            totalBetText.x = 243;
+            totalBetText.y = 300;
+
+            totalText = new createjs.Text("Total", "20px Consolas", "#FFFFFF");
+            totalText.x = 228;
+            totalText.y = 270;
+
+            remainingvalue = remainingvalue - 30;
+
+            accountText = new createjs.Text(remainingvalue.toString(), "20px Consolas", "#FFFFFF");
+            accountText.x = 65;
+            accountText.y = 238;
+
+            stage.addChild(accountText);
+            stage.addChild(totalText);
+
+            stage.addChild(totalBetText);
+            stage.addChild(bit30Text);
+        }
+        else {
+            alert("You have no money");
+        }
     }
 }
 function doBet10() {
-
-    if (totalBetValue < defaultamount) {
-        totalBetValue += 10;
-
-        stage.removeChild(accountText);
-        stage.removeChild(totalBetText);
-        stage.removeChild(bit10Text);
-
-        minBet += 10;
-        bit10Text = new createjs.Text(minBet.toString(), "20px Consolas", "#FFFFFF");
-        bit10Text.x = 190;
-        bit10Text.y = 238;
-
-        totalBetText = new createjs.Text(totalBetValue.toString(), "20px Consolas", "#FFFFFF");
-        totalBetText.x = 243;
-        totalBetText.y = 300;
-
-        totalText = new createjs.Text("Total", "20px Consolas", "#FFFFFF");
-        totalText.x = 228;
-        totalText.y = 270;
-
-        var remainingamount = defaultamount - totalBetValue;
-
-        accountText = new createjs.Text(remainingamount.toString(), "20px Consolas", "#FFFFFF");
-        accountText.x = 65;
-        accountText.y = 238;
-
-        stage.addChild(accountText);
-
-        stage.addChild(totalText);
-
-        stage.addChild(totalBetText);
-
-        stage.addChild(bit10Text);
+    if (timerstatus == true) {
+        alert("Wait until Spinning Stops");
     }
     else {
-        alert("You have no money");
+        if (totalBetBeforeReset < defaultamount) {
+            totalBetValue += 10;
+            totalBetBeforeReset += 10;
+            stage.removeChild(accountText);
+            stage.removeChild(totalBetText);
+            stage.removeChild(bit10Text);
+
+            minBet += 10;
+            bit10Text = new createjs.Text(minBet.toString(), "20px Consolas", "#FFFFFF");
+            bit10Text.x = 190;
+            bit10Text.y = 238;
+
+            totalBetText = new createjs.Text(totalBetValue.toString(), "20px Consolas", "#FFFFFF");
+            totalBetText.x = 243;
+            totalBetText.y = 300;
+
+            totalText = new createjs.Text("Total", "20px Consolas", "#FFFFFF");
+            totalText.x = 228;
+            totalText.y = 270;
+
+            remainingvalue = remainingvalue - 10;
+
+            accountText = new createjs.Text(remainingvalue.toString(), "20px Consolas", "#FFFFFF");
+            accountText.x = 65;
+            accountText.y = 238;
+
+            stage.addChild(accountText);
+
+            stage.addChild(totalText);
+
+            stage.addChild(totalBetText);
+
+            stage.addChild(bit10Text);
+        }
+        else {
+            alert("You have no money");
+        }
     }
 }
 function createImage(imagename,spin) {
@@ -361,35 +386,48 @@ function createImage(imagename,spin) {
         obj = new createjs.Bitmap("assets/images/"+imagename);
         obj.x = x;
         obj.y = y;
-        stage.addChild(obj);   
+        stage.addChild(obj);
+        if (spin == 0)
+            fruit1 = obj;
+        if (spin == 1)
+            fruit2 = obj;   
+        if (spin == 2)
+            fruit3 = obj;
 }
 
-var arr = new Array();
-arr[0] = "blank.png";
-arr[1] = "banana.png";
-arr[2] = "bar.png";
-arr[3] = "bigwin.png";
-arr[4] = "eggplant.png";
-arr[5] = "lemon.png";
-arr[6] = "melon.png";
-arr[7] = "orange.png";
-arr[8] = "pee.png";
-arr[9] = "seven.png";
-var rollcur =   Math.floor((Math.random() * 9));
-var timer1,timer2;
-var timerstatus = false;
-var x1 = 0;
-var y1 = 347;
-var objj, obj1, obj2;
+
 function roller() {
-    if (totalBetValue == 0) {
+    
+    if (remainingvalue == 0 && totalBetValue == 0) {
+        alert("You have no Money. Please Reset");
+    }
+    else if (totalBetValue == 0) {
         alert("Please Bet");
     }
     else {
+        stage.removeChild(winvalueText);
+        winvaluetxt = 0;                
+        winvalueText = new createjs.Text(winvaluetxt.toString(), "20px Consolas", "#FFFFFF");
+        winvalueText.x = 425;
+        winvalueText.y = 238;
+        stage.addChild(winvalueText);
 
-
+        var arr = new Array();
+        arr[0] = "blank.png";
+        arr[1] = "banana.png";
+        arr[2] = "bar.png";
+        arr[3] = "bigwin.png";
+        arr[4] = "eggplant.png";
+        arr[5] = "lemon.png";
+        arr[6] = "melon.png";
+        arr[7] = "orange.png";
+        arr[8] = "pee.png";
+        arr[9] = "seven.png";
 
         if (timerstatus == false) {
+            var x1 = 0;
+            var y1 = 347;
+
             timerstatus = true;
             stage.removeChild(winningText);
             winningText = new createjs.Text("........Spinning.........", "25px Consolas", "#FFFFFF");
@@ -397,7 +435,9 @@ function roller() {
             winningText.y = 540;
             stage.addChild(winningText);
 
-           
+            stage.removeChild(fruit1);
+            stage.removeChild(fruit2);
+            stage.removeChild(fruit3);
 
             timer1 = setInterval(function () {
                 if (y1 + 15 > 465)
@@ -428,7 +468,7 @@ function roller() {
                 stage.addChild(obj2);
 
                 y1 = y1 + 15;
-            }, 100);
+            }, 50);
 
             timer2 = setTimeout(function () {
                 stage.removeChild(objj);
@@ -439,10 +479,94 @@ function roller() {
                 clearInterval(timer1);
                 clearTimeout(timer2);
 
-            }, 8000);
+            }, 5000);
         }
         else {
             alert("Already Spinning");
         }
     }
+}
+function calulateWIN() {
+    if (winArray[0] == winArray[1] && winArray[0] == winArray[2]) {
+
+        stage.removeChild(winningText);
+        winningText = new createjs.Text("You Won", "40px Consolas", "#FFFFFF");
+        winningText.x = 160;
+        winningText.y = 540;
+        stage.addChild(winningText);        
+
+        var calcamount = 0;
+        var winFruit = winArray[0];
+        if (winFruit == "banana") {
+            calcamount = totalBetValue * bananaScore;
+            remainingvalue = remainingvalue + calcamount;
+        }
+        else if (winFruit == "bar") {
+            calcamount = totalBetValue * barScore;
+            remainingvalue = remainingvalue + calcamount;
+        }
+        else if (winFruit == "bigwin") {
+            calcamount = jackpot;
+            remainingvalue = remainingvalue + jackpot;
+            stage.removeChild(winningText);
+            winningText = new createjs.Text("JJAACCKKPPOOOOT", "40px Consolas", "#FFFFFF");
+            winningText.x = 170;
+            winningText.y = 540;
+            stage.addChild(winningText);
+        }
+        else if (winFruit == "eggplant") {
+            calcamount = totalBetValue * eggplantScore;
+            remainingvalue = remainingvalue + calcamount;
+        }
+        else if (winFruit == "lemon") {
+            calcamount = totalBetValue * lemonScore;
+            remainingvalue = remainingvalue + calcamount;
+        }
+        else if (winFruit == "melon") {
+            calcamount = totalBetValue * melonScore;
+            remainingvalue = remainingvalue + calcamount;
+        }
+        else if (winFruit == "orange") {
+            calcamount = totalBetValue * orangeScore;
+            remainingvalue = remainingvalue + calcamount;
+        }
+        else if (winFruit == "pee") {
+            calcamount = totalBetValue * peeScore;
+            remainingvalue = remainingvalue + calcamount;
+        }
+        else if (winFruit == "seven") {
+            calcamount = totalBetValue * sevenScore;
+            remainingvalue = remainingvalue + calcamount;
+        }
+
+        stage.removeChild(winvalueText);
+
+        winvaluetxt = calcamount;
+
+        winvalueText = new createjs.Text(winvaluetxt.toString(), "20px Consolas", "#FFFFFF");
+        winvalueText.x = 425;
+        winvalueText.y = 238;
+        stage.addChild(winvalueText);
+
+    }
+    else {
+        winArray = new Array();
+        stage.removeChild(winningText);
+        winningText = new createjs.Text("You Loose", "40px Consolas", "#FFFFFF");
+        winningText.x = 170;
+        winningText.y = 540;
+        stage.addChild(winningText);
+    }
+    winArray = new Array();
+    minBet = 0;
+    maxBet = 0;
+    totalBetValue = 0;
+
+        
+    stage.removeChild(bit30Text);
+    stage.removeChild(bit10Text);
+    stage.removeChild(totalBetText);
+    stage.removeChild(totalText);
+
+
 }
